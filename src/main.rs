9ff5a691395a164ee83bd2baf6de2ce8a150362f
Geo Turcsanyi <georgiana@ik.me>
2025-10-21 20:53:14 +0200

    version where nothing works

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,61 @@
 use libc::c_int;
 use nix::libc;
 use nix::sched::clone;
-use nix::sys::signal::Signal::SIGCHLD;
+use nix::sys::signal::Signal::{SIGCHLD, SIGHUP};
+use nix::sys::wait::wait;
+use nix::unistd::ForkResult::{Child, Parent};
+use nix::unistd::{fork, getpid, getppid};
 use std::env::args;
 use std::process::{Command, exit};
 
-const STACK_SIZE: usize = 1024 * 1024; //1mb
+const STACK_SIZE: usize = 10 * 1024 * 1024; // 10mb
+
 
 fn run() -> isize {
-    let args: Vec<String> = args().collect();
-    if args.len() <= 2 {
-        eprintln!("specify command to run")
-    }
-
-    // let cmd = &args[2];
-    // let cmd_args = &args[3..];
-    // let _ = Command::new(cmd)
-    //     .args(cmd_args)
-    //     .spawn()
-    //     .unwrap()
-    //     .wait()
-    //     .expect("failed to execute process");
-
-    println!("a child process");
+    let _ = Command::new("/bin/bash")
+        .args(["-i"])
+        .spawn()
+        .unwrap()
+        .wait()
+        .expect("failed to execute process");
     0
 }
 
 fn main() {
-    let args: Vec<String> = args().collect();
-    if args.len() < 2 {
-        eprintln!("pass them arguments");
-        exit(1);
-    }
+    // This yells illegal instruction
     unsafe {
-        match args[1].as_str() {
-            "run" => {
-                let stack: &mut [u8; STACK_SIZE] = &mut [0; STACK_SIZE];
-                let signal: Option<c_int> = Some(SIGCHLD as c_int);
+        let stack: &mut [u8; STACK_SIZE] = &mut [0; STACK_SIZE];
+        let signal: Option<c_int> = Some(SIGCHLD as c_int);
 
-                let child_callback: nix::sched::CloneCb = Box::new(run);
+        let child_callback: nix::sched::CloneCb = Box::new(run);
 
-                let cp = clone(
-                    child_callback,
-                    stack,
-                    nix::sched::CloneFlags::CLONE_NEWUTS,
-                    signal,
-                )
-                .expect("should be able to run");
+        let _ = clone(
+            child_callback,
+            stack,
+            nix::sched::CloneFlags::CLONE_NEWUTS,
+            signal,
+        )
+        .expect("should be able to run");
 
-                nix::sys::wait::waitpid(cp, None).expect("cannot wait");
-            }
-            _ => {
-                panic!("whyyy")
-            }
-        }
+        // nix::sys::wait::waitpid(cp, None).expect("cannot wait");
     }
-    println!("hiii")
+
+    // This works, but, as expected, no isolation
+    // let pid = unsafe { fork() };
+    //
+    // match pid.expect("Fork Failed: Unable to create child process!") {
+    //     Child => println!(
+    //         "Hello from child process with pid: {} and parent pid:{}",
+    //         getpid(),
+    //         getppid()
+    //     ),
+    //     Parent { child } => {
+    //         wait();
+    //         println!(
+    //             "Hello from parent process with pid: {} and child pid:{}",
+    //             getpid(),
+    //             child
+    //         );
+    //     }
+    // }
 }
